1a6e82bda7c6b9dc166bcfc6b45bac25d57eef45
mirae <luke@krkids.co.kr>
2022-07-27 19:01:29 +0900

    예제를 NPC로 수정해서 테스트 중

--- Assets/Scripts/FindPath/RollerAgent.cs
+++ Assets/Scripts/FindPath/RollerAgent.cs
@@ -22,11 +22,11 @@ public class RollerAgent : Agent
     /// </summary>
     public override void OnEpisodeBegin()
     {
-        if (this.transform.localPosition.y < 0)
+        if (transform.localPosition.y < 0)
         {
-            this.rigidbody.angularVelocity = Vector3.zero;
-            this.rigidbody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3(0, 0.5f, 0); 
+            rigidbody.angularVelocity = Vector3.zero;
+            rigidbody.velocity = Vector3.zero;
+            transform.localPosition = new Vector3(0, 0.5f, 0); 
         }
         Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
     }
@@ -37,13 +37,14 @@ public class RollerAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(Target.localPosition); // target위치
-        sensor.AddObservation(this.transform.localPosition); // 자신의 위치
+        sensor.AddObservation(transform.localPosition); // 자신의 위치
         
         sensor.AddObservation(rigidbody.velocity.x); // 자신의 속도
         sensor.AddObservation(rigidbody.velocity.z); // 자신의 속도
     }
 
     public float forceMultiplier = 10;
+    // public GameObject viewModal = null;
     
     /// <summary>
     /// 브레인으로 전달받은 행동을 실행하는 메소드
@@ -51,23 +52,25 @@ public class RollerAgent : Agent
     /// <param name="actionBuffers"></param>
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        rigidbody.AddForce(controlSignal * forceMultiplier);
-        
-        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
-        
+        Vector3 controlSinal = Vector3.zero;
+        controlSinal.x = actionBuffers.ContinuousActions[0];
+        controlSinal.z = actionBuffers.ContinuousActions[1];
+        rigidbody.AddForce(controlSinal * forceMultiplier);
+        transform.LookAt(Target);
+        float distanceToTarget = Vector3.Distance(transform.localPosition, Target.localPosition);
+            
         if (distanceToTarget < 1.42)
         {
             SetReward(1.0f);
             EndEpisode();
         }
-        
-        else if (this.transform.localPosition.y < 0)
+
+        // 플랫폼 밖으로 나가면 Episode 종료
+        if (transform.localPosition.y < 0)
         {
             EndEpisode();
         }
+        
     }
     
     // 테스트용으로 개발자가 직접 움직이는 함수
@@ -75,7 +78,7 @@ public class RollerAgent : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         //base.Heuristic(actionsOut);
-        var continuousActionsOut = actionsOut.ContinuousActions;
+        ActionSegment<float> continuousActionsOut = actionsOut.ContinuousActions;
         continuousActionsOut[0] = Input.GetAxis("Horizontal");
         continuousActionsOut[1] = Input.GetAxis("Vertical");
     }
